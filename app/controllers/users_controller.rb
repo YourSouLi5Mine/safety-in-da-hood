d10be547358a87cf273418c0dc6349bda2c83adf
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-03-07 15:26:41 -0600

    add: edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  include SessionsHelper 
-
-  before_action :require_login, only: :show
+  before_action :require_login, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   before_action :require_logout, only: [:new, :create]
 
   def new
@@ -9,15 +8,29 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user 
+    @user = current_user
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path 
+      redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to me_url
+    else
+      render :edit
     end
   end
 
@@ -32,5 +45,10 @@ class UsersController < ApplicationController
       flash[:error] = "You must be logged in to access this section"
       redirect_to login_url
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to me_url unless current_user?(@user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
+  get '/me/:id/edit', to:'users#edit', as: 'edit_me'
+  patch 'me/:id/edit', to:'users#update'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
