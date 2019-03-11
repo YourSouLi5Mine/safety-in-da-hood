38013c934bbf3e8f5e55a412c01a397cc6ed2c58
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-03-11 11:47:03 -0600

    add: follows asynchronously

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,13 +1,19 @@
 class FollowsController < ApplicationController
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to all_path
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to all_path }
+      format.js
+    end
   end
   
   def destroy
-    user = Follow.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to all_path
+    @user = Follow.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to all_path }
+      format.js
+    end
   end
 end
