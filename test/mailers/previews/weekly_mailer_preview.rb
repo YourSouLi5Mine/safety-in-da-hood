cd14d6a0da89cba107aa6900a46fb5f3424f10b2
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-03-12 16:25:37 -0600

    add: mailer

---
+++ test/mailers/previews/weekly_mailer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/weekly_mailer
+class WeeklyMailerPreview < ActionMailer::Preview
+  def weekly_preview
+    WeeklyMailer.weekly
+  end
+end
