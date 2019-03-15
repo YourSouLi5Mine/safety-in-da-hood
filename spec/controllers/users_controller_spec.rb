27f74c2306b2ee7572048bbb0dcb1daeee952da5
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-03-15 11:05:14 -0600

    add: users unit tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,22 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  before do
-  end
-
-  it { should use_before_action(:require_logout) }
-  it { should use_before_action(:require_login) }
-
-  it do
-    params = {
+  before(:all) do
+    @params = {
       user: {
         username: Faker::Internet.username,
         email:    Faker::Internet.email,
         password: Faker::Internet.password
       }
     }
-    should permit(:username, :email, :password).
-      for(:create, params: params).
-      on(:user)
   end
+
+  it { should use_before_action(:require_logout) }
+  it { should use_before_action(:require_login) }
+
+  context 'routes' do
+    it { should route(:get, '/signup').to(action: :new) }
+    it { should route(:post, '/signup').to(action: :create) }
+    it { should route(:get, '/all').to(action: :index) }
+    it { should route(:get, '/me/edit').to(action: :edit) }
+    it { should route(:patch, '/me/edit').to(action: :update) }
+  end
+
+  context 'GET #new' do
+    before { get :new }
+    it { should render_template('new') }
+    it { should respond_with(200) }
+  end
+
+  context 'POST #create' do
+    before { post :create, params: @params }
+    it { should redirect_to(root_path) }
+    it { should respond_with(302) }
+  end
+
 end
